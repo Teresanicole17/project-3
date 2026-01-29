fce8769df93edb6be8bcab13e6f1d622201bfba9
Teresanicole17 <teresa-judge@student.kirkwood.edu>
2026-01-29 08:51:05 -0600

    Add files via upload

---
+++ project3.ino
@@ -0,0 +1,27 @@
+// Project 3 - Demonstrating PWM
+int d = 5;
+void setup()
+{
+   pinMode(3, OUTPUT); // LED control pin is 3, a PWM-capable pin
+   pinMode(5, OUTPUT);
+}
+ 
+void loop()
+{
+   for ( int a = 0 ; a < 256 ; a++ )
+   {
+      analogWrite(3, a);
+      //delay(d);
+      analogWrite(5, a);
+      delay(d);
+   }
+   for ( int a = 255 ; a >= 0 ; a-- )
+   {
+      analogWrite(3, a);
+      //delay(d);
+      analogWrite(5, a);
+      delay(d);
+   }
+   delay(200);
+   
+}
